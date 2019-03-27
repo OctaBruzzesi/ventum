2c371a2374a11608dda5b6c6fecbd51eda597a96
Octa Bruzzesi <octa.bruzzesi@gmail.com>
2019-03-26 23:53:18 -0300

    ADD: water form

--- src/views/Water/WaterForm.jsx
+++ src/views/Water/WaterForm.jsx
@@ -1,84 +1,96 @@
 import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types';
 import { Field, reduxForm } from 'redux-form';
+import { withRouter } from 'react-router-dom';
 import withStyles from '@material-ui/core/styles/withStyles';
 import { compose } from 'recompose';
+import ButtonBase from '@material-ui/core/ButtonBase';
 
 import GridItem from 'components/Grid/GridItem';
 import GridContainer from 'components/Grid/GridContainer';
 import Button from 'components/CustomButtons/Button';
-import { InputText } from 'components/Form';
+import Card from 'components/Card/Card';
+import CardBody from 'components/Card/CardBody';
+import { InputText, Select } from 'components/Form';
 import { required, number, email } from '../../utils/validations';
+import provinces from '../../utils/provinces';
 
 class WaterForm extends PureComponent {
   render() {
     const {
+      history,
       handleSubmit,
       onFormSubmit,
     } = this.props;
 
     return (
-      <form onSubmit={handleSubmit(onFormSubmit.bind(this))}>
-        <GridContainer>
-          <GridItem md={12}>
-            <Field
-              name="province"
-              component={InputText}
-              validate={[required]}
-              type="text"
-              label="Provincia"
-            />
-          </GridItem>
-          <GridItem md={12}>
-            <Field
-              name="city"
-              component={InputText}
-              validate={[required]}
-              type="text"
-              label="Ciudad"
-            />
-          </GridItem>
-          <GridItem md={3}>
-            <Field
-              name="date"
-              component={InputText}
-              validate={[required]}
-              type="datetime-local"
-              label="Fecha de la muestra"
-            />
-          </GridItem>
-          <GridItem md={12}>
-            <Field
-              name="coliforms"
-              component={InputText}
-              type="number"
-              label="N° de coliformes"
-            />
-          </GridItem>
-          <GridItem md={12}>
-            <Field
-              name="notes"
-              validate={[required]}
-              component={InputText}
-              type="text"
-              label="Notas"
-            />
-          </GridItem>
-        </GridContainer>
+      <Card>
+        <CardBody>
+          <form onSubmit={handleSubmit(onFormSubmit.bind(this))}>
+            <GridContainer>
+              <GridItem md={3}>
+                <Field
+                  name="province"
+                  component={Select}
+                  validate={[required]}
+                  items={provinces}
+                  label="Provincia"
+                />
+              </GridItem>
+              <GridItem md={6}>
+                <Field
+                  name="city"
+                  component={InputText}
+                  validate={[required]}
+                  type="text"
+                  label="Ciudad/Lugar"
+                />
+              </GridItem>
+              <GridItem md={3} />
+              <GridItem md={3}>
+                <Field
+                  name="date"
+                  component={InputText}
+                  validate={[required]}
+                  type="datetime-local"
+                  label="Fecha de la muestra"
+                />
+              </GridItem>
+              <GridItem md={12}>
+                <Field
+                  name="coliforms"
+                  component={InputText}
+                  type="number"
+                  label="N° de coliformes"
+                />
+              </GridItem>
+              <GridItem md={12}>
+                <Field
+                  name="notes"
+                  validate={[required]}
+                  component={InputText}
+                  type="text"
+                  label="Notas"
+                />
+              </GridItem>
+            </GridContainer>
 
-        <GridContainer>
-          <Button
-            type="submit"
-          >
-            Ok
-          </Button>
-          <Button
-            type="danger"
-          >
-            No guardar
-          </Button>
-        </GridContainer>
-      </form>
+            <GridContainer>
+              <Button
+                type="submit"
+                color="primary"
+              >
+                Ok
+              </Button>
+              <Button
+                onClick={() => history.goBack()}
+              >
+                No guardar
+              </Button>
+            </GridContainer>
+          </form>
+        </CardBody>
+      </Card>
     );
   }
 }
@@ -92,5 +104,6 @@ export default compose(
   reduxForm({
     form: 'WaterNewForm',
   }),
+  withRouter,
   withStyles({}),
 )(WaterForm);
